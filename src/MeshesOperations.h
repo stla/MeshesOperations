b72554bc9c75744382699024b58d7a90c59de83b
stla <laurent_step@yahoo.fr>
2022-05-23 11:31:15 +0200

    gmpxx meshes

--- src/MeshesOperations.h
+++ src/MeshesOperations.h
@@ -68,7 +68,7 @@ typedef CGAL::Surface_mesh<EPoint3> EMesh3;
 typedef K::Vector_3 Vector3;
 typedef EK::Vector_3 EVector3;
 
-typedef CGAL::Cartesian<gmpxx::mpq_class> QK;
+typedef CGAL::Cartesian<mpq_class> QK;
 typedef CGAL::Surface_mesh<QK::Point_3> QMesh3;
 typedef QK::Point_3 QPoint3;
 typedef QK::Vector_3 QVector3;
