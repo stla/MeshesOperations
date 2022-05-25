4d1ed9d1cc14074d496da7ceef54f50a605eb21d
stla <laurent_step@yahoo.fr>
2022-05-25 20:35:21 +0200

    connectedComponentsK

--- src/connectedComponents.cpp
+++ src/connectedComponents.cpp
@@ -17,20 +17,20 @@ Rcpp::List connectedComponentsK(const Rcpp::List rmesh0,
   const bool really_triangulate = !isTriangle && triangulate;
   Rcpp::List cc_rmeshes(ncc);
   size_t i = 0;
-  for(Mesh3 mesh = cc_meshes.begin(); mesh != cc_meshes.end(); ++mesh){
+  for(auto mesh = cc_meshes.begin(); mesh != cc_meshes.end(); ++mesh){
     Rcpp::IntegerMatrix Edges0;
     Rcpp::NumericMatrix Normals0;
     if(really_triangulate) {
-      Edges0 = getEdges2<K, Mesh3, Point3>(mesh, epsilon);
+      Edges0 = getEdges2<K, Mesh3, Point3>(*mesh, epsilon);
       if(normals) {
-        Normals0 = getKNormals(mesh);
+        Normals0 = getKNormals(*mesh);
       }
-      const bool success = PMP::triangulate_faces(mesh);
+      const bool success = PMP::triangulate_faces(*mesh);
       if(!success) {
         Rcpp::stop("Triangulation has failed.");
       }
     }
-    Rcpp::List cc_rmesh_i = RSurfKMesh(mesh, normals, epsilon);
+    Rcpp::List cc_rmesh_i = RSurfKMesh(*mesh, normals, epsilon);
     if(really_triangulate) {
       cc_rmesh_i["edges0"] = Edges0;
       if(normals) {
