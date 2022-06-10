e1b7c3f38c3c29b5df0838c5d30a7762f0396b62
stla <laurent_step@yahoo.fr>
2022-06-10 13:58:02 +0200

    fixed output edges

--- R/meshes.R
+++ R/meshes.R
@@ -293,7 +293,7 @@ Mesh <- function(
 	edgesDF <- mesh[["edges"]]
 	mesh[["edgesDF"]] <- edgesDF
 	mesh[["edges"]] <- as.matrix(edgesDF[, c("i1", "i2")])
-	exteriorEdges <- subset(edgesDF, exterior)[, c("i1", "i2")]
+	exteriorEdges <- as.matrix(subset(edgesDF, exterior)[, c("i1", "i2")])
 	mesh[["exteriorEdges"]] <- exteriorEdges
 	mesh[["exteriorVertices"]] <- which(table(exteriorEdges) != 2L)
 	if(normals){
